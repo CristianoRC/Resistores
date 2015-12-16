161f4f6dc6960e1a741f3de5a15ec64c39c1b59f
Cristiano Raffi Cunha <Contato@cristianoprogramador.com>
2015-12-16 11:50:24 -0200

    16/12/2015 11:50
    Commit inicial

---
+++ Software Resistores/Frm_RCores.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Eletrônica_Software
+{
+    partial class Frm_RCores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_RCores));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Combo4 = new System.Windows.Forms.ComboBox();
+            this.Combo_3 = new System.Windows.Forms.ComboBox();
+            this.Combo_2 = new System.Windows.Forms.ComboBox();
+            this.Combo_1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.Cinta4 = new System.Windows.Forms.PictureBox();
+            this.Cinta3 = new System.Windows.Forms.PictureBox();
+            this.Cinta2 = new System.Windows.Forms.PictureBox();
+            this.Cinta1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Lbl_Resultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Combo4);
+            this.groupBox1.Controls.Add(this.Combo_3);
+            this.groupBox1.Controls.Add(this.Combo_2);
+            this.groupBox1.Controls.Add(this.Combo_1);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.Cinta4);
+            this.groupBox1.Controls.Add(this.Cinta3);
+            this.groupBox1.Controls.Add(this.Cinta2);
+            this.groupBox1.Controls.Add(this.Cinta1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(558, 217);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cores do  Resistor";
+            // 
+            // Combo4
+            // 
+            this.Combo4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Combo4.FormattingEnabled = true;
+            this.Combo4.Items.AddRange(new object[] {
+            "Ouro",
+            "Prata",
+            "Nenhum"});
+            this.Combo4.Location = new System.Drawing.Point(406, 143);
+            this.Combo4.Name = "Combo4";
+            this.Combo4.Size = new System.Drawing.Size(91, 21);
+            this.Combo4.TabIndex = 9;
+            this.Combo4.SelectedIndexChanged += new System.EventHandler(this.Combo4_SelectedIndexChanged);
+            // 
+            // Combo_3
+            // 
+            this.Combo_3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Combo_3.FormattingEnabled = true;
+            this.Combo_3.Items.AddRange(new object[] {
+            "Amarelo",
+            "Azul",
+            "Branco",
+            "Cinza",
+            "Laranja",
+            "Marrom",
+            "Preto",
+            "Verde",
+            "Vermelho",
+            "Violeta"});
+            this.Combo_3.Location = new System.Drawing.Point(285, 143);
+            this.Combo_3.Name = "Combo_3";
+            this.Combo_3.Size = new System.Drawing.Size(91, 21);
+            this.Combo_3.TabIndex = 8;
+            this.Combo_3.SelectedIndexChanged += new System.EventHandler(this.Combo_3_SelectedIndexChanged);
+            // 
+            // Combo_2
+            // 
+            this.Combo_2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Combo_2.FormattingEnabled = true;
+            this.Combo_2.Items.AddRange(new object[] {
+            "Amarelo",
+            "Azul",
+            "Branco",
+            "Cinza",
+            "Laranja",
+            "Marrom",
+            "Preto",
+            "Verde",
+            "Vermelho",
+            "Violeta"});
+            this.Combo_2.Location = new System.Drawing.Point(171, 143);
+            this.Combo_2.Name = "Combo_2";
+            this.Combo_2.Size = new System.Drawing.Size(91, 21);
+            this.Combo_2.TabIndex = 7;
+            this.Combo_2.SelectedIndexChanged += new System.EventHandler(this.Combo_2_SelectedIndexChanged);
+            // 
+            // Combo_1
+            // 
+            this.Combo_1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Combo_1.FormattingEnabled = true;
+            this.Combo_1.Items.AddRange(new object[] {
+            "Amarelo",
+            "Azul",
+            "Branco",
+            "Cinza",
+            "Laranja",
+            "Marrom",
+            "Preto",
+            "Verde",
+            "Vermelho",
+            "Violeta"});
+            this.Combo_1.Location = new System.Drawing.Point(61, 143);
+            this.Combo_1.Name = "Combo_1";
+            this.Combo_1.Size = new System.Drawing.Size(91, 21);
+            this.Combo_1.TabIndex = 1;
+            this.Combo_1.SelectedIndexChanged += new System.EventHandler(this.Combo_1_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Eletrônica_Software.Properties.Resources.rNada;
+            this.pictureBox2.Location = new System.Drawing.Point(292, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Eletrônica_Software.Properties.Resources.resr;
+            this.pictureBox6.Location = new System.Drawing.Point(307, 30);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(24, 90);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Cinta4
+            // 
+            this.Cinta4.Image = global::Eletrônica_Software.Properties.Resources.rNada;
+            this.Cinta4.Location = new System.Drawing.Point(302, 30);
+            this.Cinta4.Name = "Cinta4";
+            this.Cinta4.Size = new System.Drawing.Size(5, 90);
+            this.Cinta4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cinta4.TabIndex = 4;
+            this.Cinta4.TabStop = false;
+            // 
+            // Cinta3
+            // 
+            this.Cinta3.Image = global::Eletrônica_Software.Properties.Resources.rNada;
+            this.Cinta3.Location = new System.Drawing.Point(282, 30);
+            this.Cinta3.Name = "Cinta3";
+            this.Cinta3.Size = new System.Drawing.Size(10, 90);
+            this.Cinta3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cinta3.TabIndex = 3;
+            this.Cinta3.TabStop = false;
+            // 
+            // Cinta2
+            // 
+            this.Cinta2.Image = global::Eletrônica_Software.Properties.Resources.rNada;
+            this.Cinta2.Location = new System.Drawing.Point(272, 30);
+            this.Cinta2.Name = "Cinta2";
+            this.Cinta2.Size = new System.Drawing.Size(10, 90);
+            this.Cinta2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cinta2.TabIndex = 2;
+            this.Cinta2.TabStop = false;
+            // 
+            // Cinta1
+            // 
+            this.Cinta1.Image = global::Eletrônica_Software.Properties.Resources.rNada;
+            this.Cinta1.Location = new System.Drawing.Point(262, 30);
+            this.Cinta1.Name = "Cinta1";
+            this.Cinta1.Size = new System.Drawing.Size(10, 90);
+            this.Cinta1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cinta1.TabIndex = 1;
+            this.Cinta1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Eletrônica_Software.Properties.Resources.resl;
+            this.pictureBox1.Location = new System.Drawing.Point(239, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Lbl_Resultado
+            // 
+            this.Lbl_Resultado.AutoSize = true;
+            this.Lbl_Resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Resultado.ForeColor = System.Drawing.Color.DarkRed;
+            this.Lbl_Resultado.Location = new System.Drawing.Point(257, 273);
+            this.Lbl_Resultado.Name = "Lbl_Resultado";
+            this.Lbl_Resultado.Size = new System.Drawing.Size(36, 18);
+            this.Lbl_Resultado.TabIndex = 1;
+            this.Lbl_Resultado.Text = "0 Ω,";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Resultado:";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(470, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Frm_RCores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(637, 314);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lbl_Resultado);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Frm_RCores";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo de Resistencia";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cinta1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox Cinta4;
+        private System.Windows.Forms.PictureBox Cinta3;
+        private System.Windows.Forms.PictureBox Cinta2;
+        private System.Windows.Forms.PictureBox Cinta1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox Combo4;
+        private System.Windows.Forms.ComboBox Combo_3;
+        private System.Windows.Forms.ComboBox Combo_2;
+        private System.Windows.Forms.ComboBox Combo_1;
+        private System.Windows.Forms.Label Lbl_Resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Software Resistores/Frm_RCores.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Eletrônica_Software
+{
+    public partial class Frm_RCores : Form
+    {
+        public Frm_RCores()
+        {
+            InitializeComponent();
+        }
+
+        #region ComboBox 1 Cor
+        private void Combo_1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Cor_C1 = null;
+
+            if (string.IsNullOrEmpty(Combo_1.SelectedItem.ToString()))
+            {
+                MessageBox.Show("Por favor insira o valor na 1ª caixa de texto", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Cor_C1 = Combo_1.SelectedItem.ToString();
+            }
+
+
+            switch (Cor_C1)
+            {
+
+                case "Preto":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rPreto;
+
+                    break;
+                case "Marrom":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rMarrom;
+
+                    break;
+                case "Vermelho":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rVermelho;
+
+                    break;
+                case "Laranja":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rLaranja;
+
+                    break;
+                case "Amarelo":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rAmarelo;
+
+                    break;
+                case "Verde":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rVerde;
+
+                    break;
+                case "Azul":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rAzul;
+
+                    break;
+                case "Violeta":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rVioleta;
+
+                    break;
+                case "Cinza":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rCinza;
+
+                    break;
+                case "Branco":
+                    Cinta1.Image = Eletrônica_Software.Properties.Resources.rBranco;
+
+                    break;
+            }
+        }
+
+
+        #endregion
+
+
+        #region ComboBox 2 Cor 
+        private void Combo_2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Cor_C2 = null;
+
+            if (string.IsNullOrEmpty(Combo_2.SelectedItem.ToString()))
+            {
+                MessageBox.Show("Por favor insira o valor na 2ª caixa de texto", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                Cor_C2 = Combo_2.SelectedItem.ToString();
+            }
+
+            switch (Cor_C2)
+            {
+
+                case "Preto":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rPreto;
+
+                    break;
+                case "Marrom":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rMarrom;
+
+                    break;
+                case "Vermelho":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rVermelho;
+
+                    break;
+                case "Laranja":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rLaranja;
+
+                    break;
+                case "Amarelo":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rAmarelo;
+
+                    break;
+                case "Verde":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rVerde;
+
+                    break;
+                case "Azul":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rAzul;
+
+                    break;
+                case "Violeta":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rVioleta;
+
+                    break;
+                case "Cinza":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rCinza;
+
+                    break;
+                case "Branco":
+                    Cinta2.Image = Eletrônica_Software.Properties.Resources.rBranco;
+
+                    break;
+            }
+        }
+
+        #endregion
+
+
+        #region ComboBox 3 Cor
+        private void Combo_3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Cor_C3 = null;
+
+            if (string.IsNullOrEmpty(Combo_3.SelectedItem.ToString()))
+            {
+                MessageBox.Show("Por favor insira o valor na 3ª caixa de texto", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Combo_3.SelectedItem.ToString();
+            }
+
+            switch (Cor_C3)
+            {
+
+                case "Preto":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rPreto;
+
+                    break;
+                case "Marrom":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rMarrom;
+
+                    break;
+                case "Vermelho":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rVermelho;
+
+                    break;
+                case "Laranja":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rLaranja;
+
+                    break;
+                case "Amarelo":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rAmarelo;
+
+                    break;
+                case "Verde":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rVerde;
+
+                    break;
+                case "Azul":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rAzul;
+
+                    break;
+                case "Violeta":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rVioleta;
+
+                    break;
+                case "Cinza":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rCinza;
+
+                    break;
+                case "Branco":
+                    Cinta3.Image = Eletrônica_Software.Properties.Resources.rBranco;
+
+                    break;
+            }
+        }
+
+        #endregion
+
+
+        #region ComboBox 4 Cor
+        private void Combo4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string Cor_C4 = null;
+
+            if (string.IsNullOrEmpty(Combo4.SelectedItem.ToString()))
+            {
+                MessageBox.Show("Por favor insira o valor na 4ª caixa de texto", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                Combo4.SelectedItem.ToString();
+            }
+
+            switch (Cor_C4)
+            {
+
+                case "Ouro":
+                    Cinta4.Image = Eletrônica_Software.Properties.Resources.rOuro;
+
+                    break;
+                case "Prata":
+                    Cinta4.Image = Eletrônica_Software.Properties.Resources.rPrata;
+
+                    break;
+                case "Nenhum":
+                    Cinta4.Image = Eletrônica_Software.Properties.Resources.rNada;
+
+                    break;
+            }
+
+        }
+
+        #endregion
+
+        //-------------------------------------- Botão Calcular -----------------------------------------//
+        #region Botão para Calcular
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Double Resultado = 0;
+            string Tolerancia = "";
+
+            try
+            {
+                string Cor_C4 = Combo4.SelectedItem.ToString();
+                string Cor_C3 = Combo_3.SelectedItem.ToString();
+                string Cor_C2 = Combo_2.SelectedItem.ToString();
+                string Cor_C1 = Combo_1.SelectedItem.ToString();
+
+                #region Calculando Tolerancia
+                switch (Cor_C4)
+                {
+
+                    case "Ouro":
+                        Tolerancia = "± 5%";
+
+                        break;
+                    case "Prata":
+                        Tolerancia = "± 10%";
+
+                        break;
+                    case "Nenhum":
+                        Tolerancia = "± 20%";
+
+                        break;
+                }
+                #endregion
+
+                #region Calculando Primeiro Numero
+                //Calculando 1 Numero
+                switch (Cor_C1)
+                {
+
+                    case "Preto":
+                        Resultado = 00;
+
+                        break;
+                    case "Marrom":
+                        Resultado = 10;
+
+                        break;
+                    case "Vermelho":
+                        Resultado = 20;
+
+                        break;
+                    case "Laranja":
+                        Resultado = 30;
+
+                        break;
+                    case "Amarelo":
+                        Resultado = 40;
+
+                        break;
+                    case "Verde":
+                        Resultado = 50;
+
+                        break;
+                    case "Azul":
+                        Resultado = 60;
+
+                        break;
+                    case "Violeta":
+                        Resultado = 70;
+
+                        break;
+                    case "Cinza":
+                        Resultado = 80;
+
+                        break;
+                    case "Branco":
+                        Resultado = 90;
+
+                        break;
+                }
+                #endregion
+
+                #region Calculando Segundo Numero
+
+
+                switch (Cor_C2)
+                {
+
+                    case "Preto":
+                        Resultado += 0;
+
+                        break;
+                    case "Marrom":
+                        Resultado += 1;
+
+                        break;
+                    case "Vermelho":
+                        Resultado += 2;
+
+                        break;
+                    case "Laranja":
+                        Resultado += 3;
+
+                        break;
+                    case "Amarelo":
+                        Resultado += 4;
+
+                        break;
+                    case "Verde":
+                        Resultado += 5;
+
+                        break;
+                    case "Azul":
+                        Resultado += 6;
+
+                        break;
+                    case "Violeta":
+                        Resultado += 7;
+
+                        break;
+                    case "Cinza":
+                        Resultado += 8;
+
+                        break;
+                    case "Branco":
+                        Resultado += 9;
+
+                        break;
+                }
+
+
+                #endregion
+
+                #region Calculando Terceiro Numero
+
+
+                switch (Cor_C3)
+                {
+
+                    case "Preto":
+                        Resultado *= Math.Pow(10, 0);
+
+                        break;
+                    case "Marrom":
+                        Resultado *= Math.Pow(10, 1);
+
+                        break;
+                    case "Vermelho":
+                        Resultado *= Math.Pow(10, 2);
+
+                        break;
+                    case "Laranja":
+                        Resultado *= Math.Pow(10, 3);
+
+                        break;
+                    case "Amarelo":
+                        Resultado *= Math.Pow(10, 4);
+
+                        break;
+                    case "Verde":
+                        Resultado *= Math.Pow(10, 5);
+
+                        break;
+                    case "Azul":
+                        Resultado *= Math.Pow(10, 6);
+
+                        break;
+                    case "Violeta":
+                        Resultado *= Math.Pow(10, 7);
+
+                        break;
+                    case "Cinza":
+                        Resultado *= Math.Pow(10, 8);
+
+                        break;
+                    case "Branco":
+                        Resultado *= Math.Pow(10, 9);
+
+                        break;
+                }
+                #endregion
+
+
+                //Mandando o resultado para a varialvel
+                Lbl_Resultado.Text = Resultado.ToString() + " Ω," + Tolerancia.ToString();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show($"Ocorreu um erro: {exc.Message}", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        #endregion
+    }
+}
